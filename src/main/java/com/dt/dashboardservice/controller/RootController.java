7718fbb3b91664103724dbdf3f648bc977e30312
Gustavo Correa <gustavo.correa@sap.com>
2022-06-02 00:10:59 -0300

    a few tests to retrieve and display wells from the orchestrator microservice

--- src/main/java/com/dt/dashboardservice/controller/RootController.java
+++ src/main/java/com/dt/dashboardservice/controller/RootController.java
@@ -1,14 +1,17 @@
 package com.dt.dashboardservice.controller;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestClientException;
 
 import com.dt.dashboardservice.client.WellOrchestratorClient;
+import com.dt.dashboardservice.model.Well;
 
 @Controller
 @RequestMapping("/")
@@ -19,8 +22,18 @@ public class RootController {
 
 	@GetMapping
 	public String index(Model model) {
+
+		//TODO put this well-orchestrator call inside a service class
+		List<Well> wellList;
+		try {
+			wellList = wellOrchestratorClient.getAllWells();
+		}
+		catch (RestClientException e) {
+			wellList = new ArrayList<>();
+		}
+
 		model.addAttribute("pageTitle", "Digital Twin - Virtual Well Dashboard");
-		model.addAttribute("wells", Arrays.asList("Test Well number 1", "RJ-380", "RJ-390"));
+		model.addAttribute("wells", wellList);
 		return "index";
 	}
 
